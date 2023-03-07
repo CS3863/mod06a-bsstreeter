6e86198a74c3870ce0bc27c0c830d6400cc45460
bsstreeter <124920658+bsstreeter@users.noreply.github.com>
2023-03-07 13:47:18 -0600

    mod06-Streeter

---
+++ mod06A-Streeter/Assets/scripts/playerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed = 5.0f;
+    public float turnspeed = 45.0f;
+    public float horizontalInput;
+    public float forwardInput; 
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move vechile forward
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //move vechile forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //rotates car
+        transform.Rotate(Vector3.up, turnspeed * horizontalInput * Time.deltaTime);
+    }
+}
